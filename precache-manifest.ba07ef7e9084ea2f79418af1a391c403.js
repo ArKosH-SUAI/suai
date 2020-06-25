9ef60b99a9abcb9ab41cf5cd066ff7790c5f9e2b
ArKosH-SUAI <filipp-raevsk20@rambler.ru>
2020-06-25 22:56:41 +0300

    Updates

---
+++ precache-manifest.ba07ef7e9084ea2f79418af1a391c403.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eba8a6bf1b01b27a47d6e02b17e9801",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a8f78c3ce297a1ddb44",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "c7a49ed434eb6f8f8a86",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0a8f78c3ce297a1ddb44",
+    "url": "./static/js/2.65bf3e5b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.65bf3e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a49ed434eb6f8f8a86",
+    "url": "./static/js/main.592d18a8.chunk.js"
+  },
+  {
+    "revision": "4178ad76fd120f9433eb",
+    "url": "./static/js/runtime-main.1c2bd76c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
